21f235a15942dc82831cbc9830e6ad83368a8dc2
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-04 06:53:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -11,9 +11,10 @@ def square_array(array)
   square = []
   counter = 0
  
-  while counter < array.size do
-    square << (array[counter]**2)
+  while array[counter] do
+    square << (array[counter] ** 2)
     counter += 1
   end
+  square
 end
 
